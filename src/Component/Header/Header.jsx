bea7b36ecf102a903891cedd9c92713d876fb420
mrana565 <rana565m@gmail.com>
2023-03-31 11:02:49 +0600

    header added

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <h1>Job Instruction</h1>
+      </div>
+      <div>
+        <img src="../../assets/images/user_3.png" alt="header img" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
